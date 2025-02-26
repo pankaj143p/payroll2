c4950ac1957fbc7eba906c2d003a69021cf724bb
pankaj143p <pankaj114477pankaj@gmail.com>
2025-02-26 15:33:27 +0530

    [Pankaj] Implement in-memory storage for Employee Payroll data in Service Layer

--- src/main/java/com/bridgelabz/payroll/service/EmployeeService.java
+++ src/main/java/com/bridgelabz/payroll/service/EmployeeService.java
@@ -7,62 +7,120 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
-@Service
-// implement service class with dto
-public class EmployeeService {
-   @Autowired
-    EmployeeRepository empRes;
-   // method for find all employees
-    public List<Employee> getAllEmployees(){
-        return empRes.findAll();
-    }
-
-    // find employee by id
-    public Employee getEmployeeById(int id){
-        return empRes.findById(id).orElse(null);
-    }
-
-    // save employee
-//    public Employee saveEmployee(Employee emp){
-//        return empRes.save(emp);
+//
+//@Service
+//// implement service class with dto
+//public class EmployeeService {
+//   @Autowired
+//    EmployeeRepository empRes;
+//   // method for find all employees
+//    public List<Employee> getAllEmployees(){
+//        return empRes.findAll();
 //    }
-     public Employee saveEmployee(EmployeeDTO empDto){
-        Employee emp  = new Employee();
-        emp.setName(empDto.getName());
-        emp.setSalary(empDto.getSalary());
-        emp.setGender(empDto.getGender());
-        return empRes.save(emp);
-     }
-    // for delete employee
-    public boolean deleteEmployee(int id){
-        if(empRes.existsById(id)) {
-            empRes.deleteById(id);
-            return true;
-        }
-        return false;
-    }
-
-    // update employee details
-//    public Employee updateEmployee(int id, Employee emp){
-//        if(empRes.existsById(id)){
-//            emp.setId(id);
-//            return empRes.save(emp);
+//
+//    // find employee by id
+//    public Employee getEmployeeById(int id){
+//        return empRes.findById(id).orElse(null);
+//    }
+//
+//    // save employee
+////    public Employee saveEmployee(Employee emp){
+////        return empRes.save(emp);
+////    }
+//     public Employee saveEmployee(EmployeeDTO empDto){
+//        Employee emp  = new Employee();
+//        emp.setName(empDto.getName());
+//        emp.setSalary(empDto.getSalary());
+//        emp.setGender(empDto.getGender());
+//        return empRes.save(emp);
+//     }
+//    // for delete employee
+//    public boolean deleteEmployee(int id){
+//        if(empRes.existsById(id)) {
+//            empRes.deleteById(id);
+//            return true;
+//        }
+//        return false;
+//    }
+//
+//    // update employee details
+////    public Employee updateEmployee(int id, Employee emp){
+////        if(empRes.existsById(id)){
+////            emp.setId(id);
+////            return empRes.save(emp);
+////        }
+////        return null;
+////    }
+//
+//    public Employee updateEmployee(int id, EmployeeDTO empDto){
+//        Optional<Employee> optEmp = empRes.findById(id);
+//        if(optEmp.isPresent()){
+//            Employee emp = optEmp.get();
+//            emp.setName(empDto.getName());
+//            emp.setSalary(empDto.getSalary());
+//            empRes.save(emp);
+//            return emp;
 //        }
 //        return null;
 //    }
+//}
 
-    public Employee updateEmployee(int id, EmployeeDTO empDto){
-        Optional<Employee> optEmp = empRes.findById(id);
-        if(optEmp.isPresent()){
-            Employee emp = optEmp.get();
-            emp.setName(empDto.getName());
-            emp.setSalary(empDto.getSalary());
-            empRes.save(emp);
-            return emp;
+
+//  <-------------  Using ArrayList  ------------------>
+
+@Service
+public class EmployeeService {
+
+    // In-memory list to store employee data
+    private List<Employee> employeeList = new ArrayList<>();
+
+    // Save a new employee (store it in memory)
+    public Employee saveEmployee(EmployeeDTO employeeDTO) {
+        Employee employee = new Employee();
+        employee.setName(employeeDTO.getName());
+        employee.setSalary(employeeDTO.getSalary());
+        employee.setStartDate(LocalDate.now());  // Default start date to current date
+        employee.setGender("Not Specified");  // Default gender
+        // Add employee to in-memory list
+        employeeList.add(employee);
+        return employee;
+    }
+
+    // Get all employees from the in-memory list
+    public List<Employee> getAllEmployees() {
+        return employeeList;  // Return the list of employees
+    }
+
+    // Get employee by ID (searching through in-memory list)
+    public Employee getEmployeeById(int id) {
+        Optional<Employee> employee = employeeList.stream()
+                .filter(emp -> emp.getId() == id)
+                .findFirst();
+        return employee.orElse(null);  // Return the employee if found, otherwise return null
+    }
+
+    // Update an employee's details (modify in-memory list)
+    public Employee updateEmployee(int id, EmployeeDTO employeeDTO) {
+        Employee employee = getEmployeeById(id);
+        if (employee != null) {
+            employee.setName(employeeDTO.getName());
+            employee.setSalary(employeeDTO.getSalary());
+            return employee;
         }
-        return null;
+        return null;  // If employee not found, return null
+    }
+
+    // Delete an employee (remove from in-memory list)
+    public boolean deleteEmployee(int id) {
+        Employee employee = getEmployeeById(id);
+        if (employee != null) {
+            employeeList.remove(employee);  // Remove the employee from the list
+            return true;
+        }
+        return false;  // Return false if employee wasn't found
     }
 }
+
