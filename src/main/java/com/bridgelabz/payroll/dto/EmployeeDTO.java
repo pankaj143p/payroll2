989bf320e3c08e8957611b61493d780b847d6210
pankaj143p <pankaj114477pankaj@gmail.com>
2025-02-28 16:48:44 +0530

    [Pankaj] : bug fixes

--- src/main/java/com/bridgelabz/payroll/dto/EmployeeDTO.java
+++ src/main/java/com/bridgelabz/payroll/dto/EmployeeDTO.java
@@ -1,8 +1,7 @@
 package com.bridgelabz.payroll.dto;
 
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,17 +19,24 @@ public class EmployeeDTO {
     @Pattern(regexp = "^[A-Za-z ]+$", message = "Name must contain only letters and spaces")
     private String name;
 
-    @Min(value = 1, message = "Salary should be a positive number")
+    @Min(value = 500, message = "Salary should be more than 500")
     private double salary;
 
+    @JsonFormat(pattern = "dd MM yyyy")
+    @NotNull(message = "start date needed")
+    @PastOrPresent(message = "Start date should be past or present")
     private LocalDate startDate;
 
+    @Pattern(regexp = "male|female", message = "Gender needs male or female")
     private String gender;
 
+    @NotBlank(message = "Note should not empty")
     private String note;
 
+    @NotBlank(message = "Profile picture should be not null")
     private String profilePicture;
 
+    @NotNull(message = "department should be needed")
     private  List<String>department;
 
 
